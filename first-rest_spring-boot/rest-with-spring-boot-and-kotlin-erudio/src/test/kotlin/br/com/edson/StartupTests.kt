b8f13e0b372edc63f2b8c2f5d997a84d9519c960
Edson Siebra <edsiebra@gmail.com>
2024-07-01 20:24:21 -0300

    Primeiro commit

---
+++ first-rest_spring-boot/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/edson/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.edson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
